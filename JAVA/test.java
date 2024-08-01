de4c8a2bdaa749e3f9977a6f69a4341d57d74b97
ArelyXl <andrycaceres2006@gmail.com>
2024-08-01 16:07:43 -0500

    pom config

---
+++ JAVA/test.java
@@ -0,0 +1,8 @@
+public class test {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
